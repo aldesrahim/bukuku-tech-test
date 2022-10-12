d54e7280d9215757859c5614859eb5c913c68dd5
Al Desrahim <37100012+aldesrahim@users.noreply.github.com>
2022-10-12 16:04:29 +0700

    Bind ExchangeRateManagerInterface to ExchangeRateManager

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use App\Helper\DateTransformHelper;
+use App\Services\ExchangeRate\Contracts\ExchangeRateManagerInterface;
+use App\Services\ExchangeRate\ExchangeRateManager;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
@@ -28,6 +30,8 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         $this->setLocale();
+
+        $this->app->bind(ExchangeRateManagerInterface::class, ExchangeRateManager::class);
     }
 
     private function setLocale()
