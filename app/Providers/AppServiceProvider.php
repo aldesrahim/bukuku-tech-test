b23d692e362bf3aaccbff252a839e537329da02a
Al Desrahim <37100012+aldesrahim@users.noreply.github.com>
2022-10-12 13:14:04 +0700

    Update AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Helper\DateTransformHelper;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
+        $this->setLocale();
+    }
+
+    private function setLocale()
+    {
+        $locale = DateTransformHelper::getLocaleCode();
+
+        setlocale(LC_ALL, $locale);
+        Carbon::setLocale($locale);
     }
 }
